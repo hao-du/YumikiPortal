55a258d083fd5a0956c06329f5ff7be3fbbe2144
Hao Du <kevindu1986@outlook.com>
2016-10-02 22:04:31 +0700

    Modify project names.

---
+++ Presentation Layers/Yumiki.Web.Master/Areas/SampleArea/SampleAreaAreaRegistration.cs
@@ -0,0 +1,24 @@
+﻿using System.Web.Mvc;
+
+namespace Yumiki.Web.Master.Areas.SampleArea
+{
+    public class SampleAreaAreaRegistration : AreaRegistration 
+    {
+        public override string AreaName 
+        {
+            get 
+            {
+                return "SampleArea";
+            }
+        }
+
+        public override void RegisterArea(AreaRegistrationContext context) 
+        {
+            context.MapRoute(
+                "SampleArea_default",
+                "SampleArea/{controller}/{action}/{id}",
+                new { action = "Index", id = UrlParameter.Optional }
+            );
+        }
+    }
+}
