cfa5be88ddc657123dccb039855268a87bc44bee
Hao Du <kevindu1986@outlook.com>
2016-10-04 21:59:57 +0700

    Add Group Table Script

--- Databases/Yumiki.Databases/DB_Administration/CreateDatabase.sql
+++ Databases/Yumiki.Databases/DB_Administration/CreateDatabase.sql
@@ -5,10 +5,23 @@
 	[FirstName] [nvarchar](15) NULL,
 	[LastName] [nvarchar](15) NULL,
 	[Descriptions] [nvarchar](255) NULL,
-	[IsActive] [nchar](10) NOT NULL,
+	[IsActive] [bit] NOT NULL,
 	[CreateDate] [datetime] NOT NULL,
 	[LastUpdateDate] [datetime] NULL,
  CONSTRAINT [PK_TB_Users] PRIMARY KEY CLUSTERED 
+(
+	[ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+
+CREATE TABLE [dbo].[TB_Group](
+	[ID] [uniqueidentifier] NOT NULL,
+	[GroupName] [nvarchar](20) NOT NULL,
+	[Descriptions] [nvarchar](255) NULL,
+	[IsActive] [bit] NOT NULL,
+	[CreateDate] [datetime] NOT NULL,
+	[LastUpdateDate] [datetime] NULL,
+ CONSTRAINT [PK_TB_Group] PRIMARY KEY CLUSTERED 
 (
 	[ID] ASC
 )WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
