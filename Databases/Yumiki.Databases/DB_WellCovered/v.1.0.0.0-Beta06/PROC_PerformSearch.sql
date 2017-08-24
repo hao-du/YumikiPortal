3b70e20c9ca93b7c53dfb6e668d6434d676a4efb
hao du <kevindu1986@outlook.com>
2017-08-24 17:07:11 +0700

    p

--- Databases/Yumiki.Databases/DB_WellCovered/v.1.0.0.0-Beta06/PROC_PerformSearch.sql
+++ Databases/Yumiki.Databases/DB_WellCovered/v.1.0.0.0-Beta06/PROC_PerformSearch.sql
@@ -2,8 +2,10 @@
 GO
 
 ALTER PROCEDURE PROC_PerformSearch
-	@keywords NVARCHAR(255),
-	@userID UNIQUEIDENTIFIER
+	@keywords		NVARCHAR(255),
+	@userID			UNIQUEIDENTIFIER,
+	@currentPage	INT,
+	@pageSize		INT
 AS
 BEGIN
 	-- SET NOCOUNT ON added to prevent extra result sets from
@@ -13,39 +15,47 @@ BEGIN
 	SET @keywords = N'"' + @keywords + '*"'
 
     CREATE TABLE #TempTable (
-		ID UNIQUEIDENTIFIER,
-		AppID UNIQUEIDENTIFIER,
-		ObjectID UNIQUEIDENTIFIER,
-		LiveID UNIQUEIDENTIFIER,
-		FullTextIndex NTEXT,
+		ID				UNIQUEIDENTIFIER,
+		AppID			UNIQUEIDENTIFIER,
+		ObjectID		UNIQUEIDENTIFIER,
+		LiveID			UNIQUEIDENTIFIER,
+		FullTextIndex	NTEXT,
 		DisplayContents NTEXT,
-		UserID UNIQUEIDENTIFIER,
-		Descriptions NVARCHAR(255),
-		IsActive BIT,
-		CreateDate DATETIME,
-		LastUpdateDate DATETIME,
-		[Rank] INT
+		UserID			UNIQUEIDENTIFIER,
+		Descriptions	NVARCHAR(255),
+		IsActive		BIT,
+		CreateDate		DATETIME,
+		LastUpdateDate	DATETIME,
+		[Rank]			INT
 	)
 
-	INSERT INTO #TempTable
-	SELECT T.ID, T.AppID, T.ObjectID, T.LiveID, T.FullTextIndex
-		, T.DisplayContents, T.UserID, T.Descriptions
-		, T.IsActive, T.CreateDate, T.LastUpdateDate, I.[Rank] + 900000000
-	FROM CONTAINSTABLE(TB_Index, FullTextIndex, @keywords) I
-		INNER JOIN TB_Index T ON I.[Key] = T.ID AND T.UserID = @userID
+	DECLARE @offSetRows INT SET @offSetRows = (@currentPage - 1) * @pageSize
 
 	INSERT INTO #TempTable
-	SELECT T.ID, T.AppID, T.ObjectID, T.LiveID, T.FullTextIndex
-		, T.DisplayContents, T.UserID, T.Descriptions
-		, T.IsActive, T.CreateDate, T.LastUpdateDate, I.[Rank]
-	FROM FREETEXTTABLE(TB_Index, FullTextIndex, @keywords) I
-		INNER JOIN TB_Index T ON I.[Key] = T.ID AND T.UserID = @userID
-		LEFT JOIN #TempTable TEMP ON T.ID = TEMP.ID
-	WHERE TEMP.ID IS NULL
+	SELECT		T.ID, T.AppID, T.ObjectID, T.LiveID, T.FullTextIndex
+					, T.DisplayContents, T.UserID, T.Descriptions
+					, T.IsActive, T.CreateDate, T.LastUpdateDate, I.[Rank] + 900000000
+	FROM		CONTAINSTABLE(TB_Index, FullTextIndex, @keywords) I
+					INNER JOIN TB_Index T ON I.[Key] = T.ID AND T.UserID = @userID
 
-	SELECT T.ID, T.AppID, T.ObjectID, T.LiveID, T.FullTextIndex, T.DisplayContents
-	FROM #TempTable T
-	ORDER BY [Rank] DESC
+	INSERT INTO	#TempTable
+	SELECT		T.ID, T.AppID, T.ObjectID, T.LiveID, T.FullTextIndex
+					, T.DisplayContents, T.UserID, T.Descriptions
+					, T.IsActive, T.CreateDate, T.LastUpdateDate, I.[Rank]
+	FROM		FREETEXTTABLE(TB_Index, FullTextIndex, @keywords) I
+					INNER JOIN TB_Index T ON I.[Key] = T.ID AND T.UserID = @userID
+					LEFT JOIN #TempTable TEMP ON T.ID = TEMP.ID
+	WHERE		TEMP.ID IS NULL
+
+	-- Retrieve total count
+	SELECT COUNT(*) As TotalCount FROM #TempTable
+
+	-- Retrieve records with paging
+	SELECT		T.ID, T.AppID, T.ObjectID, T.LiveID, T.FullTextIndex, T.DisplayContents
+	FROM		#TempTable T
+	ORDER BY	[Rank] DESC
+	OFFSET		@offSetRows ROWS       -- skip rows
+	FETCH NEXT	@pageSize ROWS ONLY; -- take rows
 
 	DROP TABLE #TempTable
 END
