d21797fb343409f9037905fb55bd2fe016b22102
Hao Du <kevindu1986@outlook.com>
2018-04-17 23:19:10 +0700

    Add Angular 5 to OnTime project

---
+++ Presentation Layers/Yumiki.Web.Master/Areas/OnTime/app/main.ts
@@ -0,0 +1,5 @@
+import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+import { AppModule } from '/areas/ontime/app/app.module.js';
+
+platformBrowserDynamic().bootstrapModule(AppModule);
