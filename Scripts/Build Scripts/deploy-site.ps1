963405b51f97d926f3215ba408e940d3c95ca518
Hao Du <kevindu1986@outlook.com>
2018-06-07 23:30:46 +0700

    Finish Prepare and Deploy Script

--- Scripts/Build Scripts/deploy-site.ps1
+++ Scripts/Build Scripts/deploy-site.ps1
@@ -1,63 +1,21 @@
-﻿Param(  
-    [ValidateSet('Debug', 'Release')]
-    [string]$environment = 'Debug'
-)
-
-cls
+﻿cls
 
 Write-Host "##### YUMIKI PORTAL DEPLOYMENT TOOL #####" -ForegroundColor "Green"
 Write-Host "##### Version 1.0.0                 #####" -ForegroundColor "Green"
 Write-Host "##### Script Loading...             #####" -ForegroundColor "Green"
-Start-Sleep -m 3000
-
-# Get MsBuild.exe location
-$msbuildPath = "C:\Program Files (x86)\MSBuild\14.0\Bin\msbuild.exe"
-Set-Alias msbuild $msbuildPath
+Start-Sleep -m 2000
 
 # Get Source Code directory
-$sourceDirectory = Split-Path (Split-Path (Split-Path $MyInvocation.MyCommand.Path))
+$serverFolder = "\\SERVER-01\C$\inetpub\wwwroot\YumikiPortal"
 
 #IIS Host Folder
-$inetpub = "C:\inetpub\wwwroot\YumikiPortal"
+$localFolder = "C:\inetpub\wwwroot\YumikiPortal"
 
-# Use MsBuild to publish Master project
-Write-Host "##### Publishing Master #####" -ForegroundColor "Green"
+Write-Host "##### Copying files from local to server... #####" -ForegroundColor "Green"
 Write-Host ""
 Write-Host ""
 Start-Sleep -m 2000
-msbuild "$sourceDirectory\Presentation Layers\Yumiki.Web.Master\Yumiki.Web.Master.csproj" /p:DeployOnBuild=true /p:PublishProfile=$environment /verbosity:minimal
-
-#Array to store all project names
-$projects = @("Administration","MoneyTrace","OnTime","WellCovered")
-
-# Use MsBuild to publish all sub-projects in Areas
-ForEach ($project in $projects) {
-    Write-Host ""
-    Write-Host ""
-    Write-Host "##### Publishing $project... #####" -ForegroundColor "Green"
-    Write-Host ""
-    Write-Host ""
-    Start-Sleep -m 2000
-
-    msbuild "$sourceDirectory\Presentation Layers\Yumiki.Web.Master\Areas\$project\Yumiki.Web.$project.csproj" /p:DeployOnBuild=true /p:PublishProfile=$environment /verbosity:minimal
-}
-
-Write-Host ""
-Write-Host ""
-Write-Host "##### Moving Dlls from Areas' Bin folder to Master's Bin folder #####" -ForegroundColor "Green"
-Start-Sleep -m 2000
-
-# Copy all dlls from All Modules to Master Bin folder
-robocopy ($inetpub + '\Areas\Administration\bin') ($inetpub + '\bin') /MIR /xx /ETA /nfl
-robocopy ($inetpub + '\Areas\MoneyTrace\bin') ($inetpub + '\bin') /MIR /xx /ETA /nfl
-robocopy ($inetpub + '\Areas\WellCovered\bin') ($inetpub + '\bin') /MIR /xx /ETA /nfl
-robocopy ($inetpub + '\Areas\OnTime\bin') ($inetpub + '\bin') /MIR /xx /ETA /nfl
-
-# Remove all Area Bins after copying all Dll to Bin Folder
-Remove-Item ($inetpub + '\Areas\Administration\bin') -Force -Recurse
-Remove-Item ($inetpub + '\Areas\MoneyTrace\bin') -Force -Recurse
-Remove-Item ($inetpub + '\Areas\WellCovered\bin') -Force -Recurse
-Remove-Item ($inetpub + '\Areas\OnTime\bin') -Force -Recurse
+robocopy $localFolder $serverFolder /MIR /xx /ETA /ndl /ns
 
 Read-Host 'Press Enter to continue…' | Out-Null
 cls
